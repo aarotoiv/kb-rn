d70780280fe4842b39bf8d0845d128e22ec36cd0
aarotoiv <aaro.toivonen@gmail.com>
2020-02-11 21:21:21 +0200

    dimensions

--- components/Game.js
+++ components/Game.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, PixelRatio } from 'react-native';
 import { connect } from 'react-redux';
 import { testIt } from '../actions';
 
 class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+            screenWidth: 0,
+            screenPixelWidth: 0,
+            screenHeight: 0,
+            screenPixelHeight: 0
+        }
+    }
     componentDidMount() {
+        const {width, height} = Dimensions.get('window');
+        this.setState({
+            screenWidth: width,
+            screenPixelWidth: PixelRatio.getPixelSizeForLayoutSize(width),
+            screenHeight: height,
+            screenPixelHeight: PixelRatio.getPixelSizeForLayoutSize(height)
+        }, () => {
+            console.log(this.state);
+        });
         this.props.testIt();
     }
     render() {
